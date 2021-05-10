5f04503d25d9e021b0468374c3ef3e9bd477e084
Hp <Hp@LAPTOP-N2LMCUK2.lan>
2021-05-10 14:57:07 +0200

    soluzione

--- src/main/java/it/polito/tdp/extflightdelays/db/ExtFlightDelaysDAO.java
+++ src/main/java/it/polito/tdp/extflightdelays/db/ExtFlightDelaysDAO.java
@@ -7,10 +7,12 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import it.polito.tdp.extflightdelays.model.Airline;
 import it.polito.tdp.extflightdelays.model.Airport;
 import it.polito.tdp.extflightdelays.model.Flight;
+import it.polito.tdp.extflightdelays.model.Rotta;
 
 public class ExtFlightDelaysDAO {
 
@@ -37,9 +39,9 @@ public class ExtFlightDelaysDAO {
 		}
 	}
 
-	public List<Airport> loadAllAirports() {
+	public void loadAllAirports(Map<Integer, Airport> idMap) {
 		String sql = "SELECT * FROM airports";
-		List<Airport> result = new ArrayList<Airport>();
+		//List<Airport> result = new ArrayList<Airport>();
 
 		try {
 			Connection conn = ConnectDB.getConnection();
@@ -47,14 +49,18 @@ public class ExtFlightDelaysDAO {
 			ResultSet rs = st.executeQuery();
 
 			while (rs.next()) {
+				
+				if(!idMap.containsKey(rs.getInt("ID"))) {
 				Airport airport = new Airport(rs.getInt("ID"), rs.getString("IATA_CODE"), rs.getString("AIRPORT"),
 						rs.getString("CITY"), rs.getString("STATE"), rs.getString("COUNTRY"), rs.getDouble("LATITUDE"),
 						rs.getDouble("LONGITUDE"), rs.getDouble("TIMEZONE_OFFSET"));
-				result.add(airport);
+				idMap.put(airport.getId(), airport);
+				}
+				//result.add(airport);
 			}
 
 			conn.close();
-			return result;
+			//return result;
 
 		} catch (SQLException e) {
 			e.printStackTrace();
@@ -86,6 +92,70 @@ public class ExtFlightDelaysDAO {
 			return result;
 
 		} catch (SQLException e) {
+			e.printStackTrace();
+			System.out.println("Errore connessione al database");
+			throw new RuntimeException("Error Connection Database");
+		}
+	}
+	
+	public List<Airport> getVertici(int x, Map<Integer,Airport> idMap){
+		String sql = "SELECT a.id as id_vertice "
+				+"FROM airports a, flights f "
+				+"WHERE (a.id = f.ORIGIN_AIRPORT_ID OR a.id = DESTINATION_AIRPORT_ID) " 
+				+"GROUP BY a.id "
+				+"HAVING COUNT(DISTINCT(f.AIRLINE_ID)) > ?";
+		
+		List<Airport> result = new LinkedList<Airport>();
+		
+		try {
+			Connection conn = ConnectDB.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			st.setInt(1, x);
+			ResultSet rs = st.executeQuery();
+			
+			while(rs.next()) {
+				//vado a chiedere all'idMap di darmi l'oggetto con questo id
+				result.add(idMap.get(rs.getInt("id_vertice")));
+				//sono sicura che ci sia perchè l'idMap contiene tutti gli aereoporti
+			}
+			rs.close();
+			st.close();
+			conn.close();
+			return result;
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+			System.out.println("Errore connessione al database");
+			throw new RuntimeException("Error Connection Database");
+		}
+	}
+	//Metodo che mi ritorna una lista di rotte
+	//ritorna tutte le rotte del database, anche quelle non presenti nel grafo
+	public List<Rotta> getRotte(Map<Integer,Airport> idMap){
+		String sql = "SELECT f.ORIGIN_AIRPORT_ID AS id_a1, f.DESTINATION_AIRPORT_ID AS id_a2, COUNT(*) AS n "
+				+"FROM Flights f "
+				+"GROUP BY f.ORIGIN_AIRPORT_ID, f.DESTINATION_AIRPORT_ID";
+		List<Rotta> result = new LinkedList<Rotta>();
+		try {
+			Connection conn = ConnectDB.getConnection();
+			PreparedStatement st = conn.prepareStatement(sql);
+			ResultSet rs = st.executeQuery();
+			
+			while(rs.next()) {
+				
+			Airport sorgente = idMap.get(rs.getInt("id_a1"));
+			Airport destinazione = idMap.get(rs.getInt("id_a2"));
+			if(sorgente!=null && destinazione != null) {
+				result.add(new Rotta(sorgente,destinazione,rs.getInt("n")));
+				
+			}
+			}
+			rs.close();
+			st.close();
+		    conn.close();
+		    return result;
+	}
+		catch(SQLException e) {
 			e.printStackTrace();
 			System.out.println("Errore connessione al database");
 			throw new RuntimeException("Error Connection Database");
--- src/main/java/it/polito/tdp/extflightdelays/model/Model.java
+++ src/main/java/it/polito/tdp/extflightdelays/model/Model.java
@@ -1,5 +1,140 @@
 package it.polito.tdp.extflightdelays.model;
 
-public class Model {
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
+import org.jgrapht.Graphs;
+import org.jgrapht.event.ConnectedComponentTraversalEvent;
+import org.jgrapht.event.EdgeTraversalEvent;
+import org.jgrapht.event.TraversalListener;
+import org.jgrapht.event.VertexTraversalEvent;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+import org.jgrapht.traverse.BreadthFirstIterator;
+
+import it.polito.tdp.extflightdelays.db.ExtFlightDelaysDAO;
+
+public class Model {
+	private SimpleWeightedGraph<Airport, DefaultWeightedEdge> grafo;
+	private ExtFlightDelaysDAO dao;
+	private Map<Integer,Airport> idMap;
+	private Map<Airport,Airport> visita;
+	
+	public Model() {
+		dao = new ExtFlightDelaysDAO();
+		idMap = new HashMap<Integer,Airport>();
+		//riempio l'idMap
+		dao.loadAllAirports(idMap);
+	}
+	
+	public void creaGrafo(int x) { //x numero di compagnie aeree minime con il quale v
+		                           //dobbiamo filtrare i nostri vertici
+		grafo = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+		//non si può fare "Graphs.addAllVertices(grafo,idMap)"
+		//perchè questo è quello che facciamo quando nel grafo vogliamo avere tutti i vertici
+		//in questo caso invece vogliamo solo un sottoinsieme di vertici
+		Graphs.addAllVertices(grafo, dao.getVertici(x, idMap));
+		
+		//aggiungo gli archi
+		//l'informazione che c'è dal nodo A al nodo B può essere diversa 
+		//da quella che c'è  dal nodo B al nodo A 
+		for(Rotta r:dao.getRotte(idMap)) {
+			//controllo che la rotta sia presente nel grafo
+			if(this.grafo.containsVertex(r.getA1()) && this.grafo.containsVertex(r.getA2())) {
+				//vado a vedere se c'è già un arco tra i due nodi, 
+				//se non c'è vuol dire che è la prima volta che vedo la rotta
+				//se c'è già vuol dire che sto considerando la rotta inversa
+				DefaultWeightedEdge e = this.grafo.getEdge(r.getA1(), r.getA2());//-> nuovo arco tra A1 e A2
+				//è un arco NON ORIENTATO, mi dice semplicemente se tra quei due vertici c'è o no l'arco
+				//non è importante l'ordine dei paramentri 
+				if(e == null) {
+					//non c'è ancora un arco tra questi due vertici
+					Graphs.addEdgeWithVertices(grafo, r.getA1(), r.getA2());
+					//salvo l'arco con il rpimo peso che ho trovato
+				}else {
+					//ho già incotrato questo arco
+					//ne incremento solamente il peso
+					double pesoVecchio = this.grafo.getEdgeWeight(e);
+					double pesoNuovo = pesoVecchio + r.getN();
+					this.grafo.setEdgeWeight(e, pesoNuovo);
+				}
+				
+			}
+		}
+		System.out.println("Grafo creato");
+		System.out.println("numero Vertici: " + grafo.vertexSet().size());
+		System.out.println("numero Archi: " + grafo.edgeSet().size());
+		
+	}
+
+	public Set<Airport> getVertici() {
+		return this.grafo.vertexSet();
+	}
+    //punto d. devo fare una vista del grafo
+	
+	//Metodo che ritorni una lista di nodi che rappresentano il percorso
+	//se la lista è vuota vuol dire che il percorso tra quei due nodi non esiste	
+	public List<Airport> trovaPercorso (Airport a1, Airport a2){
+		List<Airport> percorso = new LinkedList<>();
+		
+		//visita in ampiezza
+		//creo l'iteratore
+		BreadthFirstIterator<Airport, DefaultWeightedEdge> it = new BreadthFirstIterator<>(grafo,a1);
+		
+		visita = new HashMap<>();
+		visita.put(a1, null);//radice
+		it.addTraversalListener(new TraversalListener<Airport, DefaultWeightedEdge>(){//metti le aprentesi graffe per poter aggiungere i metodi
+
+			@Override
+			public void connectedComponentFinished(ConnectedComponentTraversalEvent e) {
+				
+			}
+
+			@Override
+			public void connectedComponentStarted(ConnectedComponentTraversalEvent e) {
+				
+			}
+
+			@Override
+			public void edgeTraversed(EdgeTraversalEvent<DefaultWeightedEdge> e) {
+				//a noi interessa questo metodo
+				Airport airport1 = grafo.getEdgeSource(e.getEdge());
+				Airport airport2 = grafo.getEdgeTarget(e.getEdge());
+				
+				//vado a vedere nella mappa
+				//stiamo parlando di grafi NON ORIENTATI => dobbiamo capire quale dei due nodi abbiamo già visitato
+                if (visita.containsKey(airport1) && !visita.containsKey(airport2)) {
+                	visita.put(airport2, airport1);
+                }else if(visita.containsKey(airport2) && !visita.containsKey(airport1)) {
+                	visita.put(airport1, airport2);
+                }
+			}
+
+			@Override
+			public void vertexTraversed(VertexTraversalEvent<Airport> e) {
+				
+			}
+
+			@Override
+			public void vertexFinished(VertexTraversalEvent<Airport> e) {
+				
+			}
+			
+		});
+		while(it.hasNext()) {
+			it.next();
+		}
+		
+		percorso .add(a2);
+		Airport step = a2;
+		while(visita.get(step) != null) {
+			step = visita.get(step);
+			percorso.add(step);
+		}
+		
+		return percorso;
+	}
 }
